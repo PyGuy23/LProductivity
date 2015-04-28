9533dc932ff12a95875444b627ec9984ea690c8c
newuser <newuser@newusers-iMac.home>
2015-04-27 21:17:35 -0400

    Initial Commit

---
+++ LProductivity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LProductivity
+//
+//  Created by newuser on 4/27/15.
+//  Copyright (c) 2015 Power Streamer LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LProductivity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LProductivity
+//
+//  Created by newuser on 4/27/15.
+//  Copyright (c) 2015 Power Streamer LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
